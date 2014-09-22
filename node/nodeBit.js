23f8a2e73bf51d96e10e6a2cfb430f32c2804b12
joshWyatt <joshuawyatt123@gmail.com>
2014-09-22 11:38:29 -0700

    Write getLeftDiagonals and getRightDiagonals

--- node/nodeBit.js
+++ node/nodeBit.js
@@ -7,17 +7,50 @@ var padWithZeroes = function(bin, n){
   return bin;
 };
 
+var convertBinToDec = function(bin){
+  return parseInt(bin, 2);
+};
+
+var convertDecToBin = function(dec){
+  return dec.toString(2);
+};
+
 var getOpenSquares = function(row, n){
-  var dec = parseInt(row, 2);
+  var decRow = convertBinToDec(row);
   var openPositions = [];
   for(var i = 0; i < n; i++){
-    if( !(dec & 1<<i) ){
+    if( !(decRow & 1<<i) ){
       openPositions.push(i);
     }
   }
-
   return openPositions;
 };
+
+var getNextRow = function(row, n){
+  var decRow = convertBinToDec(row);
+  var rightDiagonals = getRightDiagonals(decRow, n);
+  var leftDiagonals = getLeftDiagonals(decRow, n);
+
+  return newRow;
+};
+
+var getLeftDiagonals = function(decRow, n){
+  var leftDiagonals = decRow << 1;
+  var leftDiagonalsBin = convertDecToBin(leftDiagonals);
+  if( leftDiagonalsBin.length > n){
+    leftDiagonalsBin = leftDiagonalsBin.slice(1);
+  }
+  return convertBinToDec(leftDiagonalsBin);
+};
+
+var getRightDiagonals = function(decRow, n){
+  var rightDiagonals = decRow >> 2;
+  var rightDiagonalsBin = convertDecToBin(rightDiagonals);
+  if( rightDiagonalsBin.length > n){
+    rightDiagonalsBin = rightDiagonalsBin.slice(1);
+  }
+  return convertBinToDec(rightDiagonalsBin);
+}
 //set new flag for each row where empty and within N
 
 var length = 0;
@@ -31,4 +64,4 @@ while( length < n ){
 
   flag *= 2;
   length++;
-}
+} 
